3e8bd32b2e95d4f6b1b6db00907ffd9ae0bd957b
Kumar Arunav <kumararunav13@gmail.com>
2020-05-26 19:23:19 +0530

    Update Webcam_Dataset.py

--- Webcam_Dataset.py
+++ Webcam_Dataset.py
@@ -5,12 +5,11 @@ cpt = 0
 vidStream = cv2.VideoCapture(0)
 while True:
     
-    ret, frame = vidStream.read() # read frame and return code.
+    ret, frame = vidStream.read() 
     
-    cv2.imshow("capture window", frame) # show image in window
+    cv2.imshow("capture window", frame) 
     
-    cv2.imwrite("./train-images/0/image%04i.jpg" %cpt, frame)    #Give path to  train-images/0/ and keep image%04i.jpg as it is in this line. Your images will be stored at train-images/0/ folder
-    cpt += 1
+    cv2.imwrite("./train-images/0/image%04i.jpg" %cpt, frame)    
     
         
 
