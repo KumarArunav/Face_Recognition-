d9731a1728864202ba367fed175842285e6554d2
Kumar Arunav <kumararunav13@gmail.com>
2020-05-26 19:32:16 +0530

    Add files via upload

---
+++ train_model.py
@@ -0,0 +1,35 @@
+import numpy as np
+import cv2
+import os
+
+import faceRecognition as fr
+print (fr)
+
+test_img=cv2.imread(r'./Arunav.jpg')  
+
+
+faces_detected,gray_img=fr.faceDetection(test_img)
+print("face Detected: ",faces_detected)
+
+
+
+faces,faceID=fr.labels_for_training_data(r'./train-images/') 
+face_recognizer=fr.train_classifier(faces,faceID)
+face_recognizer.save(r'C:\Users\Hp\Desktop\Nineleaps\trainingData.yml') 
+name={0:"Arunav"}   
+
+for face in faces_detected:
+    (x,y,w,h)=face
+    roi_gray=gray_img[y:y+h,x:x+h]
+    label,confidence=face_recognizer.predict(roi_gray)
+    print ("Confidence :",confidence)
+    print("label :",label)
+    fr.draw_rect(test_img,face)
+    predicted_name=name[label]
+    fr.put_text(test_img,predicted_name,x,y)
+
+resized_img=cv2.resize(test_img,(1000,700))
+
+cv2.imshow("face detection ", resized_img)
+cv2.waitKey(0)
+cv2.destroyAllWindows
